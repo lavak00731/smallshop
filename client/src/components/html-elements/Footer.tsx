e54839a85de2b235f04998e8e8f8c8a257959ed2
Ezequiel <eolavaca@gmail.com>
2025-09-16 16:05:40 -0300

    footer ready

--- client/src/components/html-elements/Footer.tsx
+++ client/src/components/html-elements/Footer.tsx
@@ -1,5 +1,25 @@
+import styled from "styled-components";
+import { colorPalette } from "../../styles/colorPalette";
+import { fontStack } from "../../styles/fontStack";
+
+const FooterContainer = styled.footer`
+  font-family: ${fontStack.text};
+  font-size: 1rem;
+  line-height: 1;
+  background: ${colorPalette.decorative};
+  color: ${colorPalette.white};
+  text-shadow: 1px 1px 1px ${colorPalette.black};
+  padding: 1rem;
+`
+const TextContainer = styled.p`
+  text-align: center;
+`
+
 export const Footer = () => {
+
   return (
-    <div>Footer</div>
+    <FooterContainer>
+      <TextContainer>Copyright {new Date().getFullYear()} &copy;</TextContainer>
+    </FooterContainer>    
   )
 }
