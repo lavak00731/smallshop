7a1a8a3cb95af807b8d5b2ae557dc2b647377727
lavak00731 <eolavaca@gmail.com>
2025-10-06 09:14:25 -0300

    product page in progress

--- client/src/views/Public/ProductPage.tsx
+++ client/src/views/Public/ProductPage.tsx
@@ -1,13 +1,61 @@
 import { memo } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Keyboard, A11y, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/a11y';
 import type { ProductInterface } from '../../types/ProductInterface';
 import { useLocation } from 'react-router-dom';
+import { MainLayout } from '../../layouts/MainLayout';
+import styled from 'styled-components';
+import { Heading } from '../../components/html-elements/Heading';
 
+const DataGrid = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
-export const ProductPage = () => {
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
+const Carousel = styled(Swiper)`
+  width: 100%;
+`
+
+const Product = () => {
   const location = useLocation();
   const product:ProductInterface = location.state?.product;
   console.log(product);
   return (
-    <div>ProductPage</div>
+    <MainLayout>
+      <DataGrid>
+        <Column>
+          <Heading headingTag={'h1'} text={product.title} />
+          <Carousel
+            modules={[Pagination, Keyboard, A11y, Navigation]}
+            pagination={{clickable: true}}
+            spaceBetween={50}
+            slidesPerView={1}   
+            navigation
+          >
+            {
+              product.images.map( img => <SwiperSlide key={Date.now()}><img src={img} alt=""/></SwiperSlide>)
+            }
+          </Carousel>
+        </Column>
+        <Column>
+        
+        </Column>
+      </DataGrid>
+      
+    </MainLayout>
+    
   )
 }
+
+export const ProductPage = memo(Product);
+
